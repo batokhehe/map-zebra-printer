5b1e978a2d44c23a24814c4b16361e444fc677ef
batokhehe <dhikanaufal@gmail.com>
2025-09-11 22:10:34 +0700

    fix: print code

--- app/src/main/java/com/werhoz/mapzebraprinter/view/MainActivity.java
+++ app/src/main/java/com/werhoz/mapzebraprinter/view/MainActivity.java
@@ -55,17 +55,17 @@ public class MainActivity extends AppCompatActivity {
 
         Button btnAuto = findViewById(R.id.btn_auto);
         btnAuto.setOnClickListener(view -> {
-//            if (!isBluetoothSet() || !isLastSyncSet()) {
-//                return;
-//            }
+            if (!isBluetoothSet() || !isLastSyncSet()) {
+                return;
+            }
             goToTemplateActivity("auto");
         }); //printToZebra());
 
         Button btnManual = findViewById(R.id.btn_manual);
         btnManual.setOnClickListener(view -> {
-//            if (!isBluetoothSet()) {
-//                return;
-//            }
+            if (!isBluetoothSet()) {
+                return;
+            }
             goToTemplateActivity("manual"); //printToZebra());
         });
 
