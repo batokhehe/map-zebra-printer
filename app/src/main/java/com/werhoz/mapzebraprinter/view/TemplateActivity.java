2a38f79cb07206afbb75ea8e9f85d9bacbd21210
batokhehe <dhikanaufal@gmail.com>
2025-09-11 19:50:08 +0700

    fix: remove sale print

--- app/src/main/java/com/werhoz/mapzebraprinter/view/TemplateActivity.java
+++ app/src/main/java/com/werhoz/mapzebraprinter/view/TemplateActivity.java
@@ -64,7 +64,7 @@ public class TemplateActivity extends AppCompatActivity {
     private void loadImages() {
         // Add drawable images here
         dataList.clear();
-        dataList.add(new TemplateModel(R.drawable.price_sale, "price_sale.zpl", "Price Sale"));
+//        dataList.add(new TemplateModel(R.drawable.price_sale, "price_sale.zpl", "Price Sale"));
 //        dataList.add(new TemplateModel(R.drawable.price_sale, "label_template.zpl", "Price Sale"));
         dataList.add(new TemplateModel(R.drawable.price_regular, "price_regular.zpl", "Price Regular"));
         if (type.equals("auto")) {
